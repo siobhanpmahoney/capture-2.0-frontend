f94ed25a7902da9c89d3cd481a68a438cae31b08
siobhanpmahoney <siobhan.p.mahoney@gmail.com>
2019-05-20 17:20:20 -0400

    adding regex helper functions to handle user pref for searches and UI display

---
+++ src/services/paramUtils.js
@@ -0,0 +1,56 @@
+{
+  level: "Internship|Entry+Level",
+  location: "Brooklyn%2C+NY|New+York%2C+NY|San+Francisco%2C+CA",
+  category: "Creative+%26+Design|Data+Science|Engineering|Project+%26+Product+Management"
+}
+
+//convert usr pref attributes into query params for theMuseAPI
+export const convertAttrStrForQuery = (attributeObj) => {
+  return Object.keys(attributeObj).map((k) => {
+    if (attributeObj[k] != null) {
+      return _helperConvertAttrStrForQuery(k, attributeObj[k])
+    }
+  }).filter((i) => !!i).join("&")
+}
+
+const _helperConvertAttrStrForQuery = (queryKey, attributeString) => {
+    let string = `${queryKey}=${attributeString}`
+    return string.split("|").join(`&${queryKey}=`)
+}
+
+
+
+// convert user pref attributes into arrays with readable strings for UI
+export const convertAttrStrForDisplay = (attributeObj) => {
+  let transformedObj = {}
+  Object.keys(attributeObj).map((k) => {
+    transformedObj[k] = _helperConvertAttrStrForDisplay(attributeObj[k])
+  })
+  return transformedObj
+}
+
+const _helperConvertAttrStrForDisplay = (str) => {
+  return string
+  .replace(/["+"]+/g," ")
+  .replace(/\%26+/g, "&")
+  .replace(/\%2C+/g, ",")
+  .split("|")
+}
+
+
+
+export const convertDisplayToQueryParam = (criteriaObj) => {
+  let queryStr = ""
+  Object.keys(criteriaObj).forEach((criteria) => {
+    queryStr += criteriaObj[criteria].map((c) => _helperConvertDisplayToQueryParam(criteria, c)).join(``)
+  })
+  return queryStr
+}
+
+const _helperConvertDisplayToQueryParam(param, value) => {
+  let regexed = value
+  .replace(/[\s]+/g,"+")
+    .replace(/[&]+/g, "%26")
+    .replace(/[,]+/g, "%2C")
+  return `&${param}=${regexed}`
+}
---
+++ src/services/theMuseApi.js
@@ -0,0 +1,8 @@
+import {MUSE_KEY} from './keys.js'
+
+const MUSE_API = "http://https://www.themuse.com/api/public/jobs"
+
+export const searchJobs = (param_obj) => {
+  // param_obj = {categories: "<cateogry list>", levels: "<levels>", locations: "<locations>"}
+  
+}
