d0deecfb45086caa6e960030fcc12aca0690de9c
siobhanpmahoney <siobhan.p.mahoney@gmail.com>
2019-06-29 17:14:58 -0400

    rendering job search from jobsearchcontainer -- still need to align with how JobList is rendering stuff in /home

--- src/components/searchJobs/JobSearchContainer.js
+++ src/components/searchJobs/JobSearchContainer.js
@@ -8,6 +8,8 @@ import {fetchJobAppsAction} from '../../actions'
 import {convertAttrStrForDisplay} from '../../utils/pref_regex'
 import {searchJobRequest} from '../../services/theMuseApi'
 
+import JobSearchList from './JobSearchList'
+
 // hold state —> search pref., etc.
 
 class JobSearchContainer extends React.Component {
@@ -15,6 +17,8 @@ class JobSearchContainer extends React.Component {
     super(props)
 
     this.state = {
+      resultsPageCount: 0,
+      loadingState: true,
       allJobResultsArr: [],
       jobFilterDetailsObj: {}
     }
@@ -25,49 +29,102 @@ class JobSearchContainer extends React.Component {
   }
 
   getJobSearchResult = () => {
-    let resultsCount
     const userPref = this.convertsAttrToObj()
     searchJobRequest(userPref, 1)
     .then(response => {
-      resultsCount = response.page_count
-      return response
+      console.log("step 1: get results page count")
+      console.log("response — ", response)
+      console.log(response.page_count)
+      return response.page_count
     })
-    .then(res => {
-      let i = 1
-      // capture response from API call in TEMP_VARS:
+    .then(count => {
+      this.setState({
+        resultsPageCount: count
+      }, () => this.addToStateHelper(userPref))
+    })
+  }
+
+  addToStateHelper = (userPref) => {
+    let i = 1
+    console.log("step 2: make API call for each page of results")
+    console.log("page count: ", this.state.resultsPageCount)
+    // capture response from API call in TEMP_VARS:
+
+
+    while (i < this.state.resultsPageCount+1) {
       let addingToJobListState = []
       let addingToDetailState = {}
+      searchJobRequest(userPref, i)
 
-      while (i < resultsCount) {
-        searchJobRequest(userPref, i)
-        .then(jobs => {
-          for (let j of jobs.results) {
+      // jobs = response from API request to page i
+      .then(jobs => {
+        console.log(`step 2.2: getting results from page ${i}`)
+        console.log("jobs: ", jobs)
+        for (let j of jobs.results) {
+          console.log("step 3: adding each job to list to render")
+          console.log("job getting added: ", j)
+          // saving to TEMP_VARS
+          addingToJobListState.push(j)
+          console.log("step 3.1: adding job to display state")
+          console.log(`display state after ${j.name} (id: ${j.id}) gets added`)
 
-      // saving to TEMP_VARS
-            addingToJobListState.push(j)
-            addingToDetailState[j.id] = {
-              savedStatus: !!this.props.job_apps.theMuseAppHash[j.id],
-              datePosted: Date.parse(j.publication_date)
-            }
-
+          console.log(`step 3.2: adding filtering criteria for ${j.name} (id: ${j.id}) to displayState`)
+          addingToDetailState[j.id] = {
+            savedStatus: !!this.props.job_apps.theMuseAppHash[j.id],
+            datePosted: Date.parse(j.publication_date)
           }
-        })
+
+          console.log(`${j.name} (id: ${j.id}) as saved in filter state: `, addingToDetailState[j.id])
+          console.log(`entire filter state after adding ${j.name} (id: ${j.id}): `)
+
+        }
+        return i
+      })
+      .then(res => {
+        let listStateCopy = this.state.allJobResultsArr.slice(0)
+        let filterCopy = Object.assign({}, this.state.jobFilterDetailsObj)
+        console.log(addingToJobListState)
+        console.log(addingToDetailState)
+
+        this.setState({
+          allJobResultsArr: [...listStateCopy,...addingToJobListState],
+          jobFilterDetailsObj: Object.assign({}, filterCopy, addingToDetailState)
+        }, () => console.log("Step 5000: HERE updated state"))
+        addingToJobListState = []
+        addingToDetailState = {}
+      })
+
+
         i++
-      }
 
-      // TO DO: add TEMP_VARS to state and return state
-      // this.setState({
-      //
-      // })
-      console.log("state at end", this.state)
-      return
-    })
-   }
 
+    }
+
+      // if (i > this.state.resultsPageCount) {
+        this.setState({
+          loadingState: false
+        })
+      // }
 
 
 
-// helper fn
+
+
+
+
+
+    // TO DO: add TEMP_VARS to state and return state
+    // this.setState({
+    //
+    // })
+  }
+
+
+
+
+  // helper fn
+
+
   convertsAttrToObj = () => {
     const {pref_locations, pref_categories, pref_levels} = this.props.user
     return convertAttrStrForDisplay({location: pref_locations, category: pref_categories, level: pref_levels})
@@ -76,13 +133,19 @@ class JobSearchContainer extends React.Component {
 
 
   render() {
-    console.log(this.state)
-    console.log("getting hash from redux state?", this.props.theMuseAppHash)
-    return (
-      <div className="job-search-container">
-        Job Search Container
-      </div>
-    )
+    if (!!this.state.loadingState) {
+      return (
+        <div>loading</div>
+      )
+    } else {
+      let jobs = this.state.allJobResultsArr
+      
+      return (
+        <div className="job-search-container">
+          <JobSearchList jobSearchResults = {jobs} theMuseAppHash = {this.props.job_apps.theMuseAppHash} />
+        </div>
+      )
+    }
   }
 }
 
--- src/components/searchJobs/JobSearchListItem.js
+++ src/components/searchJobs/JobSearchListItem.js
@@ -2,11 +2,12 @@ import React from 'react'
 import {withRouter, Link} from 'react-router-dom'
 
 const JobSearchListItem = (props) => {
+  debugger
   return (
     <div className="job-search-results-item-wrapper">
       <div className="job-card-main">
         <div className="job-list-item-company">
-          {props.job.company_name}
+          {props.job.company.name}
         </div>
 
         <div className="job-list-item-title">
@@ -14,7 +15,7 @@ const JobSearchListItem = (props) => {
         </div>
 
         <div className="job-list-item-location">
-          {props.job.locations}
+           {props.job.locations.map((loc) => loc.name).join(",")}
         </div>
 
       </div>
